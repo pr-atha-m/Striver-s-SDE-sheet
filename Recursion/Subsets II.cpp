bae592da49dd1eddd86c7af0d0e877e6f747a584
Pratham Bist <56242623+pr-atha-m@users.noreply.github.com>
2021-09-11 12:45:14 +0530

    Create Subsets II.cpp

---
+++ Recursion/Subsets II.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    
+    void rec(int begin , vector<int> &nums , vector<int> v , vector<vector<int>> &res){
+        res.push_back(v);
+        for(int i=begin;i<nums.size();i++){
+            if(i!=begin && nums[i] == nums[i-1]){
+                continue;
+            }
+            
+            v.push_back(nums[i]);
+            rec(i+1 , nums , v , res);
+            v.pop_back();
+        }
+        
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        
+        sort(nums.begin() , nums.end());
+        vector<vector<int>> res;
+        vector<int> v;
+        
+        rec(0 , nums , v , res);
+        return res;
+    }
+};
