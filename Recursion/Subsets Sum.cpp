679077f3e1f880b09231dfe63ccb73ec04cbcbab
Pratham Bist <56242623+pr-atha-m@users.noreply.github.com>
2021-09-11 10:44:13 +0530

    Create Subsets Sum.cpp

---
+++ Recursion/Subsets Sum.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h> 
+using namespace std; 
+
+ // } Driver Code Ends
+class Solution
+{
+public:
+    void recursive_sum(vector<int> & v ,int i ,  int N , vector<int> &arr , int sum){
+        if(i == N){
+            v.push_back(sum);
+            return;
+        }
+        
+        recursive_sum(v ,i+1 ,  N , arr , sum);
+        recursive_sum(v , i+1 , N , arr , sum+arr[i]);
+        
+        
+    }
+    vector<int> subsetSums(vector<int> arr, int N)
+    {
+        // Write Your Code here
+        vector<int> v;
+        int sum = 0;
+        recursive_sum(v ,0 ,  N , arr , sum);
+        
+        
+        return v;
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+        cin>>N;
+        vector<int> arr(N);
+        for(int i = 0 ; i < N ; i++){
+            cin >> arr[i];
+        }
+        Solution ob;
+        vector<int> ans = ob.subsetSums(arr,N);
+        sort(ans.begin(),ans.end());
+        for(auto sum : ans){
+            cout<< sum<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}  // } Driver Code Ends
